ff7fcef953ae9130162c009f20927f25fc3797bd
wanderley <2008.wsm@gmail.com>
2020-04-20 22:49:58 -0300

    Projeto node.js backen/frontend

---
+++ versao-inicial/frontend/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+	<!-- <div id="app" :class="{'hide-menu': !isMenuVisible || !user}">
+		<Header title="Cod3r - Base de Conhecimento" 
+			:hideToggle="!user"
+			:hideUserDropdown="!user" />
+		<Menu v-if="user" />
+		<Content v-else />
+		<Footer />
+	</div> -->
+
+	<div id="app" title="Cod3r - Base de Conhecimento" 	:hideToggle="!user">
+		<Header />
+		<Menu  />
+		<Content  />
+		<Footer />
+	</div>
+</template>
+
+<script>
+
+//import axios from "axios"
+//import { baseApiUrl, userKey } from "@/global"
+//import { mapState } from "vuex"
+import Header from "@/components/template/Header"
+import Menu from "@/components/template/Menu"
+import Content from "@/components/template/Content"
+import Footer from "@/components/template/Footer"
+//import Loading from "@/components/template/Loading"
+
+export default {
+	name: "App",
+	components: { Header, Menu, Content, Footer },
+}
+</script>
+
+<style>
+	* {
+		font-family: "Lato", sans-serif;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	#app {
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+
+		height: 100vh;
+		display: grid;
+		grid-template-rows: 60px 1fr 40px;
+		grid-template-columns: 300px 1fr;
+		grid-template-areas:
+			"header header"
+			"menu content"
+			"menu footer";
+	}
+
+	#app.hide-menu {
+		grid-template-areas:
+			"header header"
+			"content content"
+			"footer footer";
+	}
+</style>
---
+++ versao-inicial/frontend/src/main.js
@@ -0,0 +1,11 @@
+import 'font-awesome/css/font-awesome.css'
+
+import Vue from 'vue'
+
+import App from './App'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
